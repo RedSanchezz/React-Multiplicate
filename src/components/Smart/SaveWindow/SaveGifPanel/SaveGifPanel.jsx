ab829a09a5f4248e0b8ae0f2831ee8693076c12a
Dmitry Kulyatin <redsanchezz@yandex.ru>
2021-07-26 09:49:14 +0300

    add_gif_size_setting

--- src/components/Smart/SaveWindow/SaveGifPanel/SaveGifPanel.jsx
+++ src/components/Smart/SaveWindow/SaveGifPanel/SaveGifPanel.jsx
@@ -8,28 +8,51 @@ function SaveGifPanel(props) {
 
     const [repeat, setRepeat] = useState(0);
     const [quality, setQuality] = useState(10);
+
+
+    const [width, setWidth] = useState(props.width);
+    const [height, setHeight] = useState(props.height);
+
+
     const [background, setBackground] = useState('#ffffff');
+    const [loading, setLoading] = useState(false);
+
 
     function saveGifHandler() {
+        setLoading(true);
         let frameList = props.frameList;
         if(frameList.length===0) {
             alert('Список фреймов пуст !');
             return;
         }
-        console.log(background);
-
         let gif = new GIF({
             workers: 2,
             quality,
             repeat,
-            background: background,
-            transparent: null
+            // background: background,
+            // transparent: background,
         });
-        frameList.forEach((frame, index)=>{
-            gif.addFrame(frame.getCanvas(), {delay: frame.getDelay(), copy: true})
-        })
+
+
+
+        if(props.width != width || props.height != height){
+            frameList.forEach((frame, index)=>{
+                let canvas = document.createElement('canvas');
+                canvas.width=width;
+                canvas.height=height;
+                let ctx = canvas.getContext('2d');
+                ctx.drawImage(frame.getCanvas(), 0, 0, width, height);
+                gif.addFrame(canvas, {delay: frame.getDelay(), copy: true})
+            })
+        }
+        else {
+            frameList.forEach((frame, index)=>{
+                gif.addFrame(frame.getCanvas(), {delay: frame.getDelay(), copy: true})
+            })
+        }
+
         gif.on('finished', function(blob) {
-            alert('ПОБЕДА !')
+            setLoading(false);
             window.open(URL.createObjectURL(blob));
         });
         gif.render();
@@ -61,10 +84,32 @@ function SaveGifPanel(props) {
                         onChange={(e)=> setQuality(e.currentTarget.value)}
                     />
                 </div>
-               
             </div>
             <div className='save-gif-panel__row'>
-                <button onClick={saveGifHandler}>Сохранить !</button>
+                <div className='save-gif-panel__item'>
+                    <InputsBlock
+                        title='Ширина картинки'
+                        max='2000'
+                        min = '0'
+                        step='1'
+                        value = {width}
+                        onChange={(e)=> setWidth(e.currentTarget.value)}
+                    />
+                </div>
+                <div className='save-gif-panel__item'>
+                    <InputsBlock
+                        title='Высота'
+                        max='2000'
+                        min = '0'
+                        step='1'
+                        value = {height}
+                        onChange={(e)=> setHeight(e.currentTarget.value)}
+                    />
+                </div>
+            </div>
+            <div className='save-gif-panel__row'>
+                {loading ? <h1>Загрузка...</h1> : <button onClick={saveGifHandler}>Сохранить !</button>}
+                {/*<button onClick={saveGifHandler}>Сохранить !</button>*/}
             </div>
         </div>
     );
@@ -72,7 +117,10 @@ function SaveGifPanel(props) {
 
 function mapStateToProps(state){
     return {
-        frameList: state.frames.frameList
+        frameList: state.frames.frameList,
+        width: parseInt(state.canvas.size.width),
+        height: parseInt(state.canvas.size.height),
+
     }
 
 }
